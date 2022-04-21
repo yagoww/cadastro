f550cded993bd9cbee0c51e4365e723478780e40
Yago Araújo <95325543+yagoww@users.noreply.github.com>
2022-04-20 22:03:05 -0300

    -

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+/*! Nesta entrega, você já receberá o html e o css pronto. Sua tarefa é:
+1. Criar uma classe Pessoa que receberá as mesmas propriedades que o usuário deve preencher no formulário.
+2. Criar um novo usuário, utilizando um método estático da classe Pessoa, e adicioná-lo na lista ao lado
+3. Criar um método na classe que filtre os usuários pelo tipo de cargo. Caso o filtro seja todos, deverá renderizar todos os usuários.
+4. Não deixar que cadastre-se um novo usuário com um email já existente na lista (Exibir modal com mensagem de erro).
+*/
